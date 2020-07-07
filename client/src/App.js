06b6cad0af765a23628f98510b02e4ddf06cac7c
Anmeet <anmeet619@yahoo.com>
2020-07-07 22:34:44 +1000

    final commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,15 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alert from './components/layout/Alert';
 import { loadUser } from './actions/auth';
-import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/profile-forms/CreateProfile';
-import PrivateRoute from './components/routing/PrivateRoute';
-import EditProfile from './components/profile-forms/EditProfile';
-import AddExperience from './components/profile-forms/AddExperience';
-import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile';
-import Posts from './components/posts/Posts';
-import Post from './components/post/Post';
+import {Routes} from './components/routing/Routes';
 
 import './App.css';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import setAuthToken from './utils/setAuthToken';
-import AddEducation from './components/profile-forms/AddEducation';
+
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -37,53 +26,13 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-
+          <Switch>
           <Route exact path='/' component={Landing} />
-          <section className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register}></Route>
-              <Route exact path='/login' component={Login}></Route>
+          <Route component = {Routes} />
+          </Switch>
 
-              <Route exact path='/profiles' component={Profiles}></Route>
-              <Route exact path='/profile/:id' component={Profile}></Route>
-              <PrivateRoute
-                exact
-                path='/dashboard'
-                component={Dashboard}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/create-profile'
-                component={CreateProfile}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/edit-profile'
-                component={EditProfile}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/add-experience'
-                component={AddExperience}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/add-education'
-                component={AddEducation}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/posts'
-                component={Posts}
-              ></PrivateRoute>
-              <PrivateRoute
-                exact
-                path='/posts/:id'
-                component={Post}
-              ></PrivateRoute>
-            </Switch>
-          </section>
+         
+          
         </Fragment>
       </Router>
     </Provider>
